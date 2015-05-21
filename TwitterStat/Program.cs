e0da1fc1286cca464bc7b525f997338cc9c75c77
Matt Parnell <mparnell@gmail.com>
2015-05-21 01:05:52 -0500

    add example api keys file; turns out that tweetinvi already extracts all but emoji so i don't need the class i started

--- TwitterStat/Program.cs
+++ TwitterStat/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 using Tweetinvi;
+using Tweetinvi.Core;
+using Tweetinvi.Core.Enum;
+using Tweetinvi.Core.Exceptions;
+using Tweetinvi.Core.Interfaces;
+using Tweetinvi.Core.Interfaces.Controllers;
+using Tweetinvi.Core.Interfaces.DTO;
+using Tweetinvi.Core.Interfaces.Models;
+using Tweetinvi.Core.Interfaces.Models.Parameters;
+using Tweetinvi.Core.Interfaces.Streaminvi;
+using Tweetinvi.Core.Interfaces.WebLogic;
+using Tweetinvi.Json;
 using Tweetinvi.Streams;
+using System.IO;
+using System.Collections.Concurrent;
 
 namespace TwitterStat
 {
@@ -9,19 +22,23 @@ namespace TwitterStat
 		public static void Main (string[] args)
 		{
             int tweetsRecd = 0;
-			TwitterCredentials.SetCredentials("71941386-2e8aKSxp6NMYe0HCqGKVVdJAeeORrqByuGfxjsVJM", 
-				"0mZf8ly3bIA9upslt2ONwgqfjRVjLZpD9pVfj4yIDHIbx", 
-				"RU19mW1g4Ja3zMkU5MJpcyjPI", 
-				"JP2Hh1RWa6OAGaCCox8PTjLENio6epTOXEo8lAMPxgAUP70MbZ");
+            ConcurrentDictionary<string,int> hashtags = new ConcurrentDictionary<string,int>;
+
+            string[] apikeys = File.ReadAllLines("twitapi.txt");
+			TwitterCredentials.SetCredentials(apikeys[0], apikeys[1], apikeys[2], apikeys[3]);
 
 			Console.WriteLine ("testing...");
 
 			// Access the sample stream
 			var sampleStream = Stream.CreateSampleStream();
-			sampleStream.TweetReceived += (sender, arg) => { 
-                //Console.WriteLine(arg.Tweet.Text); 
+			sampleStream.TweetReceived += (sender, arg) => 
+            { 
+                Console.WriteLine(arg.Tweet.e); 
                 tweetsRecd++;
                 Console.WriteLine("Received {0} tweets.", tweetsRecd);
+                
+                TwitHashtag ht;
+                ht.parseHashtag(arg.Tweet.Text);
             };
 			sampleStream.StartStream();
 		}
