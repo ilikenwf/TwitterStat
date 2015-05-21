60b609e9dd6174583a535d7fa91b6bda901418f5
Matt Parnell <mparnell@gmail.com>
2015-05-21 00:37:32 -0500

    add initial class for hashtag parsing and addition to the ConcurrentDictionary; add skeleton class for emoji

---
+++ TwitterStat/TwitEmoji.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterStat
+{
+    class TwitEmoji
+    {
+        public TwitEmoji()
+        {
+            //load up the json file
+        }
+    }
+}
---
+++ TwitterStat/TwitHashtag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace TwitterStat
+{
+    class TwitHashtag
+    {
+        // parse the hashtags from our referenced tweet...no need to copy it
+        // we have to reference the ConcurrentDictionary from the main thread, so it can be updated but not overwritten
+        public void parseHashtags(ref string tweet, ref ConcurrentDictionary<string,int> hashtagContainer)
+        {
+            // Get first match.
+            Match m = Regex.Match(tweet, @"\B#\w\w+");
+
+            while (m.Success)
+            {
+                try
+                {
+                    //create/initalize or increment the counter for the hashtag slot
+                    hashtagContainer.AddOrUpdate(m.Value, 1, (key, oldValue) => oldValue + 1);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error processing hashtag {0}.'", m.Value);
+                }
+            }
+        }
+
+        // while normally we'd have a getter here for the top n values, it makes more logical sense to put that on the main thread
+        // since the main thread owns the ConcurrentDictionary containing our values and will be outputting them periodically
+    }
+}
