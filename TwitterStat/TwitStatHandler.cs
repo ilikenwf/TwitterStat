d5f64b9a814d58245088953a88d23c9e1f8d5fae
Matt Parnell <mparnell@gmail.com>
2015-05-22 04:22:27 -0500

    get emoji and url tallying working; however because twitter uses t.co we don't have an easy way of resolving domains - TODO - resolve the t.co redirects

--- TwitterStat/TwitStatHandler.cs
+++ TwitterStat/TwitStatHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -19,6 +20,10 @@ using Tweetinvi.Core.Interfaces.Streaminvi;
 using Tweetinvi.Core.Interfaces.WebLogic;
 using Tweetinvi.Json;
 using Tweetinvi.Streams;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace TwitterStat
 {
@@ -27,20 +32,17 @@ namespace TwitterStat
         //private stat storage
         private static int _tweetsRecd = 0; //number of tweets received
         private static int _tweetsProcd = 0; //number of tweets processed...debug use
-       
+        private static int _numURL = 0; //workaround because dictionary summing isn't working
+        private static JArray emoji = JArray.Parse(File.ReadAllText("emoji.json"));
+
         private ConcurrentDictionary<string, int> hashtagContainer = 
             new ConcurrentDictionary<string, int>(); //thread safe storage for our hashtag counts
-       
-        //getters
-        public int tweetsRecd
-        {
-            get { return _tweetsRecd; }
-        }
 
-        public int tweetsProcd
-        {
-            get { return _tweetsProcd; }
-        }
+        private ConcurrentDictionary<string, int> emojiContainer =
+            new ConcurrentDictionary<string, int>(); //thread safe storage for our emoji counts
+
+        private ConcurrentDictionary<string, int> tldContainer =
+            new ConcurrentDictionary<string, int>(); //thread safe storage for our tld counts
 
         public void TweetProc(Object state)
         {
@@ -52,7 +54,14 @@ namespace TwitterStat
             object[] array = state as object[];
             ITweet theTweet = (ITweet) array[0];
 
+            //process the tweet hashtags
             addHashtags(theTweet.Hashtags);
+
+            //process the tweet emoji
+            addEmoji(theTweet.Text);
+
+            //process the tweet URL's
+            addTLD(theTweet.Urls);
 
             //bump up the number of processed tweets after we have actually processed
             Interlocked.Increment(ref _tweetsProcd);
@@ -62,17 +71,50 @@ namespace TwitterStat
         public void outputStatus(object source, ElapsedEventArgs e)
         {
             var topHashtags = (from q in hashtagContainer
-                               orderby q.Value descending 
+                               orderby q.Value descending
                                select q.Key).Take(10);
 
+            var topEmoji = (from q in emojiContainer
+                               orderby q.Value descending
+                               select q.Key).Take(10);
+
+            var topDomains = (from q in tldContainer
+                            orderby q.Value descending
+                            select q.Key).Take(10);
+
+            //this is nasty but for the sake of nice output...
+            string[] ht = topHashtags.Cast<string>().ToArray();
+            string[] te = topEmoji.Cast<string>().ToArray();
+            string[] td = topDomains.Cast<string>().ToArray();
+
+            //% with urls
+            double percDone = 0;
+
+            if (_numURL > 0 && _tweetsRecd > 0)
+            {
+                percDone = Math.Round(((double)_numURL / (double) _tweetsRecd) * 100,2);
+            }
 
             //some hashtags may require a unicode compatible font for proper display
             Console.Clear();
-            Console.WriteLine("Processed {0} / {1} tweets.", _tweetsRecd, _tweetsProcd);
-            Console.WriteLine(String.Format(" {0,0} | {1,10} | {2,20} ", "Top Hashtags", "Top Emoji".PadRight(8), "Top TLDs".PadRight(22)));
+            Console.WriteLine("Processed {0} tweets.", _tweetsRecd);
 
-            foreach (string hashtag in topHashtags) {
-                Console.WriteLine(String.Format(" {0,0} | {1,10} | {2,20} ", new string(hashtag.Take(10).ToArray()).PadRight(12), "x".PadRight(10), "x"));
+            Console.WriteLine();
+            Console.WriteLine("% w/URL: {0}", percDone);
+
+            Console.WriteLine(String.Format(" {0,0} | {1,20} | {2,40} ", 
+                "Hashtags  ", "Emoji     ", "TLD's".PadRight(100)));
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string hash = (i < ht.Length) ? ht[i] : "";
+                string emo = (i < te.Length) ? te[i] : "";
+                string tld = (i < td.Length) ? td[i] : "";
+
+                Console.WriteLine(String.Format(" {0,0} | {1,20} | {2,40} ", 
+                    new string(hash.Take(10).ToArray()).PadRight(10),
+                    new string(emo.Take(20).ToArray()).PadRight(10), 
+                    tld.PadRight(40)));
             }
         }
 
@@ -88,10 +130,69 @@ namespace TwitterStat
                     }
                     catch (Exception)
                     {
+                        //haven't hit this one yet...
                         Console.WriteLine("Error processing hashtag {0}.'", hashtag.Text);
                     }
                 }
             }
+        }
+
+        private void addEmoji(string tweetText)
+        {
+            //get all of the surrogate values for this tweet
+            int[] surrogateDecimals = SurrogatesToCodePoints(tweetText);
+
+            foreach (int decValue in surrogateDecimals)
+            {
+                try { 
+                    var emojiUsed = emoji.Children().First(entry => entry["unified"].Value<string>() == decValue.ToString("X"));
+                    //create/initalize or increment the counter for the hashtag slot
+                    emojiContainer.AddOrUpdate((string) emojiUsed["name"], 1, (key, oldValue) => oldValue + 1);
+                }
+                catch
+                {
+                    //TODO: figure out what causes occasional breakage here
+                }
+            }
+        }
+
+
+        private void addTLD(List<Tweetinvi.Core.Interfaces.Models.Entities.IUrlEntity> tldList)
+        {
+            foreach (Tweetinvi.Core.Interfaces.Models.Entities.IUrlEntity url in tldList)
+            {
+                try
+                {
+                    Interlocked.Increment(ref _numURL);
+                    Uri uri = new Uri(url.URL);
+                    string domain = uri.GetLeftPart(UriPartial.Authority);
+                    tldContainer.AddOrUpdate(domain, 1, (key, oldValue) => oldValue + 1);
+                }
+                catch
+                {
+                    Console.WriteLine("Error inserting TLD.");
+                }
+            }
+        }
+
+        //this one was tough...
+        //stackoverflow saves the day again
+        //http://stackoverflow.com/questions/687359/how-would-you-get-an-array-of-unicode-code-points-from-a-net-string
+        private static int[] SurrogatesToCodePoints(string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            var codePoints = new List<int>(str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsHighSurrogate(str[i])) {
+                    codePoints.Add(Char.ConvertToUtf32(str, i));
+                    i += 1;
+                }
+            }
+
+            return codePoints.ToArray();
         }
     }
 }
